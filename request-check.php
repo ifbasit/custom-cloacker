7785ac250d4e76770aefce59d76cf47027ee65c3
Abdul Basit <49987142+ifbasit@users.noreply.github.com>
2025-07-08 15:09:28 +0500

    set dev mode with param

--- request-check.php
+++ request-check.php
@@ -60,6 +60,9 @@ set_cloacker('query_params', http_build_query($_GET) );
 set_cloacker('referrer', get_referrer() );
 set_cloacker('domain', $_SERVER['HTTP_HOST']);
 
+if (isset($_GET['ipfp_check']) && $_GET['ipfp_check'] === 'true2206') {
+    set_custom_settings('mode', 'dev');
+}
 
 
 // ==========================
